fd78ad948eaddf3421b8a2005dc614c6a217834d
Jonathan Cooper <jonathan.cooper.2010@gmail.com>
2013-10-19 21:49:14 -0500

    Tentative mail support

--- brocast/brocast.go
+++ brocast/brocast.go
@@ -10,7 +10,9 @@ import (
 
 	"appengine"
 	"appengine/datastore"
+	"appengine/taskqueue"
 	"appengine/user"
+	"appengine/mail"
 )
 
 // Message represents a message sent on Brocast.
@@ -56,11 +58,47 @@ func brocastHandler(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
+	t := taskqueue.NewPOSTTask("/mailworker", map[string][]string{"message_key": {key.String()}})
+	if _, err := taskqueue.Add(c, t, ""); err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
 	fmt.Fprintf(w, "Response received")
 
 }
 
+func mailWorker(w http.ResponseWriter, r *http.Request) {
+	c := appengine.NewContext(r)
+
+	// Get the message key from the request
+	messageKey := r.FormValue("message_key")
+	key := datastore.NewKey(c, "Message", messageKey, 0, nil)
+	c.Infof("Processing message: %v", messageKey)
+
+	// Retrieve the Message from the datastore
+	var message Message
+	if err := datastore.Get(c, key, &message); err != nil {
+		c.Errorf("%v", err)
+		return
+	}
+
+	c.Infof("Sending mail for message: %v", messageKey)
+
+	// Send an email to each recipient with a google maps link
+	msg:=&mail.Message{
+		Sender: "Brocast <jonathancooper2010@gmail.com>",
+		To: message.Recipients,
+		Subject: fmt.Sprintf("Brocast from %v", message.Account),
+		Body: message.Body,
+	}
+	if err:=mail.Send(c, msg); err!=nil{
+		c.Errorf("%v", err)
+		return
+	}
+}
+
 func init() {
 	http.HandleFunc("/brocasts", brocastHandler)
+	http.HandleFunc("/mailworker", mailWorker)
 	http.HandleFunc("/", rootHandler)
 }
